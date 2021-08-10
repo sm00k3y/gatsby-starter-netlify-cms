759dca0f8ff714f5e1ec12347861cc8b017485d5
Kuba S <kubasmolka1999@gmail.com>
2021-08-10 14:17:05 +0200

    Added google auth - version alpha

--- src/components/CommentRoll.js
+++ src/components/CommentRoll.js
@@ -6,6 +6,9 @@ export const CommentRoll = ({ id }) => {
   const [comment, setComment] = React.useState("");
   const [allComments, setAllComments] = React.useState([]);
 
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth.languageCode = "pl";
+
   const addComment = (event) => {
     console.log("I'm here!");
     if (name === "" || comment === "") {
@@ -62,8 +65,40 @@ export const CommentRoll = ({ id }) => {
     );
   };
 
+  const handleSignIn = () => {
+    firebase.auth().signInWithRedirect(provider);
+    firebase
+      .auth()
+      .getRedirectResult()
+      .then((result) => {
+        if (result.credential) {
+          /** @type {firebase.auth.OAuthCredential} */
+          var credential = result.credential;
+
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+        console.log("Successful login!!!");
+        console.log(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+  };
+
   return (
     <div>
+      <button onClick={handleSignIn}>Sign in with Google</button>
       <form onSubmit={addComment}>
         <input
           type="text"
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,16 @@
-import firebase from "firebase/app"
-import 'firebase/database'
+import firebase from "firebase/app";
+import "firebase/database";
+import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBy3f5K1jds9fvlCHyOhI8g0qEhTKIHRvg",
   authDomain: "commentsdb-77e45.firebaseapp.com",
-  databaseURL: "https://commentsdb-77e45-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL:
+    "https://commentsdb-77e45-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "commentsdb-77e45",
   storageBucket: "commentsdb-77e45.appspot.com",
   messagingSenderId: "666547385085",
-  appId: "1:666547385085:web:0261e4625013ee43b70494"
+  appId: "1:666547385085:web:0261e4625013ee43b70494",
 };
 
 firebase.initializeApp(firebaseConfig);
